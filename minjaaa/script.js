89951793004e78518db56b71c83c85d8f2391797
minja <minjababic8@gmail.com>
2021-03-10 13:51:52 +0100

    initial commit

---
+++ minjaaa/script.js
@@ -0,0 +1,26 @@
+var a = prompt("Enter first number")
+var b = prompt("Enter operation")
+var c = prompt("Enter second number")
+
+switch(b) {
+    case "+":
+        alert(parseInt(a) + parseInt(c));
+        break;
+    case "-":
+        alert(parseInt(a) - parseInt(c));
+        break;
+    case "*":
+        alert(parseInt(a) * parseInt(c));
+        break;
+    case "/":
+        alert(parseInt(a) / parseInt(c));
+        break;
+}
+
+
+    
+ 
+
+   
+
+
